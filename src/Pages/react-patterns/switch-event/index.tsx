569dc9efe90b4d708eb241d28e497471c56d3089
sergey poddubnii <poddubnii.sergey@gmail.com>
2022-12-23 22:21:48 +0200

    body

--- src/Pages/react-patterns/switch-event/index.tsx
+++ src/Pages/react-patterns/switch-event/index.tsx
@@ -4,7 +4,7 @@ import Card from '../../../Components/Card';
 const SwitchEvent = () => {
 	return <>
 		<Card title='Switch Event'>
-			<p>Switch Event</p>
+			<p>Switch Event body</p>
 		</Card>
 	</>;
 };
