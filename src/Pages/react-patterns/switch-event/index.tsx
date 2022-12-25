30d66ea717ec7495f4b3c3bd149dbc72a66d0319
sergey poddubnii <poddubnii.sergey@gmail.com>
2022-12-25 22:31:56 +0200

    add code syntax

--- src/Pages/react-patterns/switch-event/index.tsx
+++ src/Pages/react-patterns/switch-event/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Card from '../../../Components/Card';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 const SwitchEvent = () => {
 	return <>
 		<Card title='Switch Event'>
 			<h3>Switch Event body</h3>
-			<p>body</p>
+			<SyntaxHighlighter language="javascript" style={docco}>
+				{'(num) => num + 1'}
+			</SyntaxHighlighter>
 		</Card>
 	</>;
 };
