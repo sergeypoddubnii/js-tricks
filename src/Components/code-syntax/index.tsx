638e36087e5953b4aad769c0dd1ef29f9acb9a15
sergey poddubnii <poddubnii.sergey@gmail.com>
2022-12-31 00:34:58 +0200

    finish switch event pattern

--- src/Components/code-syntax/index.tsx
+++ src/Components/code-syntax/index.tsx
@@ -1,19 +1,26 @@
-import React, {FC} from 'react';
+import React, {FC, useState} from 'react';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 import {CopyToClipboard} from 'react-copy-to-clipboard';
 import { toast } from 'react-toastify';
+import './styles.scss';
 
 interface IProps {
 	code: string;
+	className?: string;
 }
 
-const CodeSyntax:FC<IProps> = ({code}:IProps) => {
+const CodeSyntax:FC<IProps> = ({code, className}:IProps) => {
+	const [copied, setCopied] = useState(false);
 
-	const handleCopyCode = () => toast.success('Copied!');
+	const handleCopyCode = () => {
+		if(copied) return;
+		setCopied(true);
+		toast.success('Copied!');
+	};
 
 	return (
-		<>
+		<div className={className ? className : 'code'}>
 			<CopyToClipboard
 				text={code}
 				onCopy={handleCopyCode}
@@ -22,7 +29,7 @@ const CodeSyntax:FC<IProps> = ({code}:IProps) => {
 					{code}
 				</SyntaxHighlighter>
 			</CopyToClipboard>
-		</>
+		</div>
 	);
 };
 
--- src/Pages/react-patterns/switch-event/index.tsx
+++ src/Pages/react-patterns/switch-event/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import Card from '../../../Components/Card';
 import CodeSyntax from '../../../Components/code-syntax';
-import codeExample from './code-example.json';
+import codeExample from './pattern.json';
+import antiPattern from './anti-pattern.json';
+import './styles.scss';
 
 const SwitchEvent = () => {
 	return <>
 		<Card title='Switch Event'>
-			<h3>Switch Event body</h3>
-			<CodeSyntax code={codeExample} />
+			<h3>Switch Event</h3>
+			<div className='switch__container'>
+				<CodeSyntax code={codeExample} />
+				<CodeSyntax code={antiPattern} />
+			</div>
 		</Card>
 	</>;
 };
