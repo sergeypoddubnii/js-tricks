da08a920dac8d668d53a658bb09e102680ad5a7b
sergey poddubnii <poddubnii.sergey@gmail.com>
2023-01-03 20:06:54 +0200

    add generics to navigation

--- src/Components/Navigation/index.tsx
+++ src/Components/Navigation/index.tsx
@@ -2,17 +2,22 @@ import React from 'react';
 import NavigationLink from './NavigationLink';
 import './styles.scss';
 import SubMenu from '../SubMenu';
+import routes from '../../routes';
 
 const Navigation = () => {
 	const reactPatterns = [{
-		route: '/react-patterns-switch-event',
+		route: routes.REACT_PATTERNS.SWITCH_EVENT,
 		name: 'Switch Event'
+	}];
+
+	const typescript = [{
+		route: routes.TYPESCRIPT.GENERICS,
+		name: 'Generics'
 	}];
 
 	return (
 		<nav>
 			<ul className='nav'>
-				<NavigationLink text='React Patterns' to={'/'}/>
 				<NavigationLink text='DevTools' to={'/'}/>
 				<NavigationLink text='Design Pattens' to={'/'}/>
 				<NavigationLink text='MV Pattens' to={'/'}/>
@@ -20,6 +25,10 @@ const Navigation = () => {
 				<SubMenu
 					title='React Patterns'
 					subList={reactPatterns}
+				/>
+				<SubMenu
+					title='Typescript'
+					subList={typescript}
 				/>
 			</ul>
 		</nav>
