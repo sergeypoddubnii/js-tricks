86ea35951550a61b5a67c5e54fc0f24e7a374ab4
sergey poddubnii <poddubnii.sergey@gmail.com>
2023-01-20 19:55:50 +0200

    add open close SOLID

---
+++ src/Pages/solid/open-close/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Card from 'Components/card';
+
+const SingleResponsibility = () => {
+
+	return (
+		<Card title='Open Close'>
+			<p>open-close</p>
+		</Card>
+	);
+};
+
+export default SingleResponsibility;
