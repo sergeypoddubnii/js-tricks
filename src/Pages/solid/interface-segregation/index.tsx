ac63cbedb2c98945b764a917f5b28bbec7cd8c7f
sergey poddubnii <poddubnii.sergey@gmail.com>
2023-01-26 22:18:25 +0200

    add ISP

--- src/Pages/solid/interface-segregation/index.tsx
+++ src/Pages/solid/interface-segregation/index.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import Card from 'Components/card';
+import ComparisonCode from '../../../Components/code-syntax/comparison-code';
+import CodeSyntax from '../../../Components/code-syntax';
+import pattern from './pattern.json';
+import antiPattern from './anti-pattern.json';
 
 const InterfaceSegregation = () => {
 
 	return (
 		<Card title='Interface segregation'>
-			<p>Interface segregation</p>
+			<p>Clients should not be forced to depend upon interfaces that they do not use</p>
+			<p>React perspective: Components should not depend on things they don’t need</p>
+			<p>Many client-specific interfaces are better than one general-purpose interface.</p>
+			<ComparisonCode>
+				<CodeSyntax code={antiPattern}/>
+				<CodeSyntax code={pattern}/>
+			</ComparisonCode>
 		</Card>
 	);
 };
