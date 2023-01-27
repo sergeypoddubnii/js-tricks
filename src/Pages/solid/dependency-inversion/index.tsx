e0edaf5b4440d5d4a5d53190b30fff5c73e8e698
sergey poddubnii <poddubnii.sergey@gmail.com>
2023-01-27 23:11:57 +0200

    add description DIP

--- src/Pages/solid/dependency-inversion/index.tsx
+++ src/Pages/solid/dependency-inversion/index.tsx
@@ -6,6 +6,10 @@ const DependencyInversion = () => {
 	return (
 		<Card title='Dependency inversion'>
 			<p>Dependency inversion</p>
+			<p>React perspective: No component or function should care about how a particular thing is done.</p>
+			<p>In most cases, if you are violating the single-responsibility principle, then you might also be violating the dependency inversion principle</p>
+			<p>For any component, look into the import section at the top. If you are importing some library that’s not responsible for displaying something (e.g. a toast or modal), then you might be violating the principle</p>
+
 		</Card>
 	);
 };
