c29da408aa709732388769721f51a1d728308b34
sergey poddubnii <poddubnii.sergey@gmail.com>
2023-01-29 21:19:13 +0200

    LSP add description

--- src/Pages/solid/dependency-inversion/index.tsx
+++ src/Pages/solid/dependency-inversion/index.tsx
@@ -5,7 +5,7 @@ const DependencyInversion = () => {
 
 	return (
 		<Card title='Dependency inversion'>
-			<p>Dependency inversion</p>
+			<p>Always have high-level code interface with an abstraction, rather than an implementation detail</p>
 			<p>React perspective: No component or function should care about how a particular thing is done.</p>
 			<p>In most cases, if you are violating the single-responsibility principle, then you might also be violating the dependency inversion principle</p>
 			<p>For any component, look into the import section at the top. If you are importing some library that’s not responsible for displaying something (e.g. a toast or modal), then you might be violating the principle</p>
