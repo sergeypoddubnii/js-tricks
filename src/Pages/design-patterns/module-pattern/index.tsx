4914f89eb580c98deae76a44941932b1fb7b709e
sergey poddubnii <poddubnii.sergey@gmail.com>
2023-02-02 16:05:10 +0200

    add module patterns description and example

--- src/Pages/design-patterns/module-pattern/index.tsx
+++ src/Pages/design-patterns/module-pattern/index.tsx
@@ -1,10 +1,13 @@
 import React, {FC} from 'react';
 import Card from 'Components/card';
+import CodeSyntax from '../../../Components/code-syntax';
+import pattern from './pattern.json';
 
 const ModulePattern:FC = () => {
 	return (
 		<Card title='Module patterns'>
-			<p>Module patterns</p>
+			<p>The module pattern is a design pattern used for improving the maintainability and reusability of the code by creating public and private access levels. Sometimes called encapsulation, it protects the value inside a module from being accessed from other scopes.</p>
+			<CodeSyntax code={pattern}/>
 		</Card>
 	);
 };
