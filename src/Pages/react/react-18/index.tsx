73a77fd84e7b4eb4f11e915e5f9756274861822d
sergey poddubnii <poddubnii.sergey@gmail.com>
2023-02-06 22:07:20 +0200

    add react 18 description

--- src/Pages/react/react-18/index.tsx
+++ src/Pages/react/react-18/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import Card from 'Components/card';
+import codeExample from './react-18-code-example.json';
+import CodeSyntax from 'Components/code-syntax';
 
 const React18 = () => {
 	return (
 		<Card title='React v18'>
-			<p>React v18</p>
-			<p>
-				link:
-				<a href={'https://www.freecodecamp.org/news/react-18-new-features/'} target='_blank' rel="noreferrer">article</a>
-			</p>
+			<h6>Automatic Batching</h6>
+			<p>Batching is when React groups multiple state updates into a single re-render for better performance</p>
+			<CodeSyntax code={codeExample.automaticBatching} />
+			<h6>A transition is a new concept in React to distinguish between urgent and non-urgent updates.</h6>
+			<p>A transition is a new concept in React to distinguish between urgent and non-urgent updates.</p>
+			<CodeSyntax code={codeExample.transition} />
 		</Card>
 	);
 };
