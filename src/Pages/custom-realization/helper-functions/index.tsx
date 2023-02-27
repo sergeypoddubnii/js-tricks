eb828991134d463faf532c4f474b8144eb7396ce
sergey poddubnii <poddubnii.sergey@gmail.com>
2023-02-27 22:55:01 +0200

    add memoize

--- src/Pages/custom-realization/helper-functions/index.tsx
+++ src/Pages/custom-realization/helper-functions/index.tsx
@@ -6,7 +6,10 @@ import functions from './functions.json';
 const HelperFunctions = () => {
 	return (
 		<Card title='Helper functions'>
+			<h6>Debounce</h6>
 			<CodeSyntax code={functions.debounce}/>
+			<h6>Memoize function</h6>
+			<CodeSyntax code={functions.memoize}/>
 		</Card>
 	);
 };
