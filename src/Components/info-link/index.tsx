15ac69448e93a8bf4c792622729c164db4b1a57d
sergey poddubnii <poddubnii.sergey@gmail.com>
2023-02-28 18:22:44 +0200

    add info link

---
+++ src/Components/info-link/index.tsx
@@ -0,0 +1,21 @@
+import React, {FC} from 'react';
+
+interface IProps {
+	path: string;
+	text?: string;
+}
+
+const InfoLink:FC<IProps> = ({path, text}:IProps) => {
+	const cutString = (string:string, length:number):string => `${string.slice(0, length)}...`;
+	return (
+		<a
+			href={path}
+			target='_blank'
+			rel="noreferrer"
+		>
+			[{' '}{text ? text : cutString(path, 50)}{' '}]
+		</a>
+	);
+};
+
+export default InfoLink;
