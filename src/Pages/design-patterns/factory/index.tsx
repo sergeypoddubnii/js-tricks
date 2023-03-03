28f2006a74bda60e3342b43f05724510ccd87bb6
sergey poddubnii <poddubnii.sergey@gmail.com>
2023-03-03 17:18:57 +0200

    add liskov

---
+++ src/Pages/design-patterns/factory/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Card from 'Components/card';
+
+const Factory = () => {
+	return (
+		<Card title={'Factory'}>
+			<p>
+				In React, the Factory pattern is a way of encapsulating the process of creating components. It involves creating a factory function that returns a component based on certain parameters that are passed to it.
+				The Factory pattern is often used in large-scale applications where there are many similar components that need to be created dynamically based on different requirements. By using a factory function, developers can avoid duplicating code and instead create a reusable and modular codebase.
+			</p>
+		</Card>
+	);
+};
+
+export default Factory;
--- src/Pages/solid/liskov-substitution/index.tsx
+++ src/Pages/solid/liskov-substitution/index.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import Card from 'Components/card';
+import CodeSyntax from 'Components/code-syntax';
+import code from './pattern.json';
 
 const LiskovSubstitution = () => {
 
 	return (
 		<Card title='Liskov Substitution'>
 			<p>Subclasses should be substitutable for their superclasses</p>
-			<p>React perspective: Components should abide by some kind of contract</p>
+			<p>
+				The Liskov Substitution Principle (LSP) is one of the SOLID principles of software development. It states that any instance of a base class should be able to be replaced by an instance of any of its derived classes without altering the correctness of the program.
+				In the context of React, this principle is relevant in the design and implementation of components. When creating components, its important to ensure that they can be easily extended and modified without breaking the functionality of the application.
+				One way to apply the LSP in React is to use inheritance and composition appropriately. Inheritance allows you to create a base component that can be extended by derived components with additional functionality, while composition allows you to build complex components by combining simpler components.
+				To ensure that components adhere to the LSP, you should avoid violating the contract of the base component. For example, if a base component expects certain props to be passed to it, a derived component should not require additional props that are not part of the contract.
+				You should also avoid overriding methods or properties of the base component in a way that alters their behavior. This can cause unexpected issues when the derived component is used in place of the base component.
+				In React, the Liskov Substitution Principle can be applied to components. In particular, it means that a child component should be able to substitute its parent component without breaking the functionality of the parent component.
+				In summary, the Liskov Substitution Principle is an important principle in software development, including React. By designing components that adhere to the LSP, you can create more flexible and maintainable code that can be easily extended and modified without breaking the functionality of the application.
+			</p>
+			<CodeSyntax code={code.pattern} />
 		</Card>
 	);
 };
