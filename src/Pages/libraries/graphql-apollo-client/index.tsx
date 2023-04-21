555313cc173ea78aaccd8f713c14470b16b6fc35
sergey poddubnii <poddubnii.sergey@gmail.com>
2023-04-21 21:20:58 +0300

    add title

--- src/Pages/libraries/graphql-apollo-client/index.tsx
+++ src/Pages/libraries/graphql-apollo-client/index.tsx
@@ -11,6 +11,7 @@ const references = [
 const GraphqlApolloClient = () => {
 	return (
 		<Section title={'GraphQL Apollo Client'}>
+			<p>Example with REST Request to graphQL server</p>
 			<CodeSyntax code={'const query = `query Todos {\n  todos {\n    data{\n      id\n      title\n      completed\n    }\n  }\n}`; // gql schema\n\nconst url = "https://graphqlzero.almansi.me/api"; //url to graphQL\n\nconst data = {\n  method: "POST", // ALL requests(query, mutations) to GraphQL server use POST\n  headers: {\n    "Content-type": "aplication/json"\n  },\n  body: JSON.stringify({ query: query }) // always use query even for mutation\n};\n\nfetch(url, data)\n  .then((response) => response.json())\n  .then((todos) => console.log("todos", todos))\n  .catch((error) => console.error(error));\n'}/>
 			<ReferenceList references={references}/>
 		</Section>
