99e04e56de0f13e998377538d51f84fb1e5d8a09
sergey poddubnii <poddubnii.sergey@gmail.com>
2023-04-26 22:18:17 +0300

    add EventEmitter

---
+++ src/Pages/design-patterns/event-emitter/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Section from 'Components/section';
+
+const EventEmitter = () => {
+	return (
+		<Section title={'Event Emitter'}>
+			<p>Event Emitter</p>
+		</Section>
+	);
+};
+
+export default EventEmitter;
--- src/Pages/design-patterns/index.tsx
+++ src/Pages/design-patterns/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import PageLayout from 'core/layout/page-layout';
 import Observer from './observer';
+import EventEmitter from './event-emitter';
 
 const DesignPatterns = () => {
 	return (
 		<PageLayout title={'Design Patterns'}>
 			<Observer/>
+			<EventEmitter/>
 		</PageLayout>
 	);
 };
